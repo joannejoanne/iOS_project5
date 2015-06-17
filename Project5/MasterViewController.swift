db80c5433a0facbfaa386c5e74f3fec7d87ac2d0
joanne <jojoannexo@gmail.com>
2015-06-17 12:19:56 +0530

    reading start.txt into array, randomly choosing word, initializing & starting game

--- Project5/MasterViewController.swift
+++ Project5/MasterViewController.swift
@@ -10,8 +10,10 @@ import UIKit
 
 class MasterViewController: UITableViewController {
 
-    var objects = [AnyObject]()
-
+    var objects = [String]()
+    // for our array of eight letter words
+    var allWords = [String]()
+    
 
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -20,7 +22,18 @@ class MasterViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-
+        // find path of file
+        if let startWordsPath = NSBundle.mainBundle().pathForResource("start", ofType: "txt") {
+            // load contents of file into a string
+            if let startWords = NSString(contentsOfFile: startWordsPath, usedEncoding: nil, error: nil) {
+                // split string into array of strings based on line break
+                allWords = startWords.componentsSeparatedByString("\n") as [String]
+            }
+        }
+        else {
+            allWords = ["silkworm"]
+        }
+        startGame() 
     }
 
     override func didReceiveMemoryWarning() {
@@ -42,11 +55,20 @@ class MasterViewController: UITableViewController {
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as UITableViewCell
 
-        let object = objects[indexPath.row] as NSDate
-        cell.textLabel!.text = object.description
+        let object = objects[indexPath.row]
+        cell.textLabel!.text = object
         return cell
     }
 
+    // when we generate new word to work with
+    func startGame() {
+        allWords.shuffle()
+        title = allWords[0]
+        // use objects array to store player's answers so far
+        objects.removeAll(keepCapacity: true)
+        // tableView comes from UITableViewController which MasterViewController inherits
+        tableView.reloadData()
+    }
 
 }
 
